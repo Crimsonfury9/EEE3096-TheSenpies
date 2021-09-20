4f2f8d8e6f1007295235d5e967356e729bb4da7a
Crimsonfury9 <crimsonfury9@gmail.com>
2021-09-20 22:59:19 +0200

    fix: againsadna

--- Prac 3/WorkPackage3/p3.py
+++ Prac 3/WorkPackage3/p3.py
@@ -117,7 +117,7 @@ def setup():
     ledPWM = GPIO.PWM(LED_accuracy,1)
 
     # Setup debouncing and callbacks
-    GPIO.add_event_detect(btn_increase, GPIO.FALLING, callback=btn_increase_pressed, bouncetime=100)
+    GPIO.add_event_detect(btn_increase, GPIO.FALLING, callback=btn_increase_pressed, bouncetime=800)
     GPIO.add_event_detect(btn_submit, GPIO.FALLING, callback=btn_guess_pressed, bouncetime=200)
     pass
 
